88352d8cdbe361e9aea1427ab857dc7455557286
Heinrich van Vught <heinrich.van.vught@gmail.com>
2017-09-06 10:26:31 +0100

    Simple c# web server~

---
+++ SimpleWebHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleWebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webServer = new SimpleHttpServer("c:\\simpleWeb", 9050);
+            //webServer.
+            Console.WriteLine("Hosting c:\\simpleWeb on port 9050" );
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
